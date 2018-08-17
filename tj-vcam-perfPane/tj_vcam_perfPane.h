a1422b4cec9b12c8c0c016251855dad038ea67c7
Jacky <tj328111241@gmail.com>
2018-08-17 18:14:06 +0800

    ...

---
+++ tj-vcam-perfPane/tj_vcam_perfPane.h
@@ -0,0 +1,15 @@
+//
+//  tj_vcam_perfPane.h
+//  tj-vcam-perfPane
+//
+//  Created by Jacky Pro on 2018/8/17.
+//  Copyright © 2018年 tajoy. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface tj_vcam_perfPane : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ tj-vcam-perfPane/tj_vcam_perfPane.m
@@ -0,0 +1,17 @@
+//
+//  tj_vcam_perfPane.m
+//  tj-vcam-perfPane
+//
+//  Created by Jacky Pro on 2018/8/17.
+//  Copyright © 2018年 tajoy. All rights reserved.
+//
+
+#import "tj_vcam_perfPane.h"
+
+@implementation tj_vcam_perfPane
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
