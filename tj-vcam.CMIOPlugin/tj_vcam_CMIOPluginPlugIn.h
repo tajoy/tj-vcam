91c0c7c7edfd6a2acc5cd1cfbf2b40d056f68d3d
Jacky <tj328111241@gmail.com>
2018-08-18 17:59:30 +0800

    ...

---
+++ tj-vcam.CMIOPlugin/tj_vcam_CMIOPluginPlugIn.h
@@ -0,0 +1,14 @@
+//
+//  tj_vcam_CMIOPluginPlugIn.h
+//  tj-vcam.CMIOPlugin
+//
+//  Created by Jacky Pro on 2018/8/18.
+//  Copyright © 2018年 tajoy. All rights reserved.
+//
+
+#import <CoreMediaIO/CMIOHardwarePlugIn.h>
+
+
+CMIOHardwarePlugInRef tj_vcam_CMIOPlugIn(CFAllocatorRef allocator, CFUUIDRef requestedTypeUUID);
+
+
---
+++ tj-vcam.CMIOPlugin/tj_vcam_CMIOPluginPlugIn.m
@@ -0,0 +1,16 @@
+//
+//  tj_vcam_CMIOPluginPlugIn.m
+//  tj-vcam.CMIOPlugin
+//
+//  Created by Jacky Pro on 2018/8/18.
+//Copyright © 2018年 tajoy. All rights reserved.
+//
+
+// It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering
+#import <OpenGL/CGLMacro.h>
+
+#import "tj_vcam_CMIOPluginPlugIn.h"
+
+CMIOHardwarePlugInRef tj_vcam_CMIOPlugIn(CFAllocatorRef allocator, CFUUIDRef requestedTypeUUID) {
+    
+}
